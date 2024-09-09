6b986713d2dd7dcba59d062650dbff7e095556cd
Nischal876 <nischalaryal11@gmail.com>
2024-09-09 13:53:13 +0545

    Utilized '@Pointcut' annotation to define pointcut for the Aspect - Changing the pointcut will be easier - Reduced repetative pointcut values for before and after advice

--- src/main/java/com/nischal/aopdemo/aop/EmployeeAspect.java
+++ src/main/java/com/nischal/aopdemo/aop/EmployeeAspect.java
@@ -4,6 +4,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -12,22 +13,28 @@ import java.util.Date;
 @Component
 public class EmployeeAspect {
 
-    @Before(value = "execution(* com.nischal.aopdemo.controller.EmployeeController.*(..))")
+    @Pointcut("execution(* com.nischal.aopdemo.controller.EmployeeController.*(..))")
+    private void forEmployeeController() {}
+
+    @Pointcut("execution(* com.nischal.aopdemo.service.EmployeeService.*(..))")
+    private void forEmployeeService() {}
+
+    @Before(value = "forEmployeeController()")
     public void beforeAdvice(JoinPoint joinPoint) {
         System.out.println("Request to " + joinPoint.getSignature() + " started at " + new Date());
     }
 
-    @After(value = "execution(* com.nischal.aopdemo.controller.EmployeeController.*(..))")
+    @After(value = "forEmployeeController()")
     public void afterAdvice(JoinPoint joinPoint) {
         System.out.println("Request to " + joinPoint.getSignature() + " ended at " + new Date());
     }
 
-    @Before(value = "execution(* com.nischal.aopdemo.service.EmployeeService.*(..))")
+    @Before(value = "forEmployeeService()")
     public void beforeAdviceForService(JoinPoint joinPoint) {
         System.out.println("Request to service layer " + joinPoint.getSignature() + " started at " + new Date());
     }
 
-    @After(value = "execution(* com.nischal.aopdemo.service.EmployeeService.*(..))")
+    @After(value = "forEmployeeService()")
     public void afterAdviceForService(JoinPoint joinPoint) {
         System.out.println("Request to service layer " + joinPoint.getSignature() + " ended at " + new Date());
     }
