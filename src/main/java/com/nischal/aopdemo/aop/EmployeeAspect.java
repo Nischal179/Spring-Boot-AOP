cbe87e0adcca8f4184d9dbc3862cf92cfd686557
Nischal876 <nischalaryal11@gmail.com>
2024-09-10 13:31:04 +0545

    Add @AfterReturning advice for EmployeeService#getAllEmployees - Log method execution and returned EmployeeResponseDTO list.

--- src/main/java/com/nischal/aopdemo/aop/EmployeeAspect.java
+++ src/main/java/com/nischal/aopdemo/aop/EmployeeAspect.java
@@ -1,15 +1,14 @@
 package com.nischal.aopdemo.aop;
 
+import com.nischal.aopdemo.dto.EmployeeResponseDTO;
 import com.nischal.aopdemo.entity.Employee;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Aspect
@@ -68,5 +67,17 @@ public class EmployeeAspect {
     @After(value = "forEmployeeService()")
     public void afterAdviceForService(JoinPoint joinPoint) {
         System.out.println("Request to service layer " + joinPoint.getSignature() + " ended at " + new Date());
+    }
+
+    @AfterReturning(pointcut = "execution(* com.nischal.aopdemo.service.EmployeeService.getAllEmployees())",
+            returning = "result")
+    public void afterReturningAdviceforService(JoinPoint joinPoint, List<EmployeeResponseDTO> result) {
+
+        // print out which method we are advising on
+        String method = joinPoint.getSignature().toShortString();
+        System.out.println("Executing @AfterReturning on method: " + method);
+
+        // print out the result of the method call
+        System.out.println("Result is : " + result);
     }
 }
