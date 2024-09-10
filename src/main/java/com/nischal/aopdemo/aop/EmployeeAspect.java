3ae5f5dcf075c4286434c9f6e119eadb04c4b15a
Nischal876 <nischalaryal11@gmail.com>
2024-09-10 10:21:07 +0545

    Code to display method signature and method's argument was added in EmployeeAspect class

--- src/main/java/com/nischal/aopdemo/aop/EmployeeAspect.java
+++ src/main/java/com/nischal/aopdemo/aop/EmployeeAspect.java
@@ -1,13 +1,16 @@
 package com.nischal.aopdemo.aop;
 
+import com.nischal.aopdemo.entity.Employee;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Objects;
 
 @Aspect
 @Component
@@ -19,8 +22,14 @@ public class EmployeeAspect {
     @Pointcut("execution(* com.nischal.aopdemo.service.EmployeeService.*(..))")
     private void forEmployeeService() {}
 
+
+// In this method I am displaying called method's signature
     @Before(value = "forEmployeeController()")
     public void beforeAdvice(JoinPoint joinPoint) {
+
+        // Method signature
+        System.out.println("Method: " + (MethodSignature)joinPoint.getSignature());
+
         System.out.println("Request to " + joinPoint.getSignature() + " started at " + new Date());
     }
 
@@ -29,8 +38,30 @@ public class EmployeeAspect {
         System.out.println("Request to " + joinPoint.getSignature() + " ended at " + new Date());
     }
 
+    // Method for logging any methods of EmployeeService class
+    // as well as display the arguments of the called method
     @Before(value = "forEmployeeService()")
     public void beforeAdviceForService(JoinPoint joinPoint) {
+
+        // Display method arguments
+
+        // get arguments
+        Object[] args = joinPoint.getArgs();
+
+        // loop through arguments
+        for (Object tempArg : args) {
+            System.out.println(tempArg);
+
+            if (tempArg instanceof Employee) {
+
+                // downcast and print Employee specific stuff
+                Employee theEmployee = (Employee) tempArg;
+
+                System.out.println("Employee name: " + theEmployee.getName());
+                System.out.println("Employee id: " + theEmployee.getId());
+            }
+        }
+
         System.out.println("Request to service layer " + joinPoint.getSignature() + " started at " + new Date());
     }
 
